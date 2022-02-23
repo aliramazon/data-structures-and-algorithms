aa589b345135d7d24a07a63bf3969043de42161b
Ali Ramazon <mr.safarali@gmail.com>
2022-02-23 12:09:20 -0700

    Implement delete method | BinaryMaxHeap

--- data-structures/09_BinaryMaxHeap.js
+++ data-structures/09_BinaryMaxHeap.js
@@ -27,6 +27,30 @@ class BinaryMaxHeap {
         return Math.floor((childIdx - 1) / 2);
     }
 
+    #calculateLargerChildIdx(trickleNodeIdx) {
+        let leftChildIdx = this.#leftChildIdx(trickleNodeIdx);
+        let rightChildIdx = this.#rightChildIdx(trickleNodeIdx);
+        let finalCompareIdx;
+
+        if (
+            (this.#data[rightChildIdx] || this.#data[rightChildIdx] === 0) &&
+            this.#data[rightChildIdx] > this.#data[leftChildIdx]
+        ) {
+            finalCompareIdx = rightChildIdx;
+        } else {
+            finalCompareIdx = leftChildIdx;
+        }
+        if (
+            (this.#data[finalCompareIdx] ||
+                this.#data[finalCompareIdx] === 0) &&
+            this.#data[finalCompareIdx] > this.#data[trickleNodeIdx]
+        ) {
+            return finalCompareIdx;
+        }
+
+        return trickleNodeIdx;
+    }
+
     insert(value) {
         this.#data.push(value);
         let newNodeIdx = this.length - 1;
@@ -45,4 +69,25 @@ class BinaryMaxHeap {
             parentIdx = this.#parentIdx(newNodeIdx);
         }
     }
+
+    delete() {
+        let largestValue = this.#data[0];
+        let lastNode = this.#data.pop();
+        if (!this.length) return lastNode;
+        this.#data[0] = lastNode;
+        let trickleNodeIdx = 0;
+        let largerChildIdx = this.#calculateLargerChildIdx(trickleNodeIdx);
+
+        while (trickleNodeIdx !== largerChildIdx) {
+            [this.#data[trickleNodeIdx], this.#data[largerChildIdx]] = [
+                this.#data[largerChildIdx],
+                this.#data[trickleNodeIdx]
+            ];
+            trickleNodeIdx = largerChildIdx;
+            largerChildIdx = this.#calculateLargerChildIdx(trickleNodeIdx);
+        }
+        return largestValue;
+    }
 }
+
+const maxHeap = new BinaryMaxHeap();

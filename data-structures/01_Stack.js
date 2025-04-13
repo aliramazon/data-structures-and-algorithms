a122fdc4300ea080840a779be12af8cbe80ef417
Ali Ramazon <mr.safarali@gmail.com>
2025-04-13 17:17:15 -0600

    Write time complexity for each method

--- data-structures/01_Stack.js
+++ data-structures/01_Stack.js
@@ -4,27 +4,27 @@ class Stack {
         this.#stack = [];
     }
 
-    /* Get the size of stack */
+    /* Get the size of stack: O(1) */
     get size() {
         return this.#stack.length;
     }
 
-    /* Checks whether the stack is empty? */
+    /* Checks whether the stack is empty:  O(1) */
     isEmpty() {
         return this.#stack.length === 0;
     }
 
-    /* Get the top item from the stack */
+    /* Get the top item from the stack: O(1) */
     peek() {
         return this.#stack[this.#stack.length - 1];
     }
 
-    /* Insert an element into the stack */
+    /* Insert an element into the stack: O(1) */
     push(x) {
         this.#stack.push(x);
     }
 
-    /* Delete an element from the stack. */
+    /* Delete an element from the stack: O(1) */
     pop() {
         if (this.isEmpty()) {
             return false;

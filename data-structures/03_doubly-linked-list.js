a5c32e839e85a99552d2e1769ec4aa48b71c3d54
Ali Ramazon <mr.safarali@gmail.com>
2025-04-14 17:47:36 -0600

    Improve doubly linked list

--- data-structures/03_doubly-linked-list.js
+++ data-structures/03_doubly-linked-list.js
@@ -18,11 +18,13 @@ class DoublyLinkedList {
         return this.#length;
     }
 
+    /* O(1) */
     push(value) {
         let newNode = new Node(value);
 
         if (this.#length === 0) {
-            this.head = this.tail = newNode;
+            this.tail = newNode;
+            this.head = this.tail;
         } else {
             this.tail.next = newNode;
             newNode.prev = this.tail;
@@ -31,6 +33,7 @@ class DoublyLinkedList {
         return ++this.#length;
     }
 
+    /* O(1) */
     pop() {
         let popped = this.tail;
         if (!popped) {
@@ -38,7 +41,8 @@ class DoublyLinkedList {
         }
 
         if (this.#length === 1) {
-            this.head = this.tail = null;
+            this.head = null;
+            this.tail = null;
         } else {
             this.tail.prev.next = null;
             this.tail = this.tail.prev;
@@ -48,6 +52,7 @@ class DoublyLinkedList {
         return popped;
     }
 
+    /* O(1) */
     unshift(value) {
         let newNode = new Node(value);
 
@@ -61,20 +66,66 @@ class DoublyLinkedList {
         return ++this.#length;
     }
 
+    /* O(1) */
     shift() {
         let shiftedNode = this.head;
         if (!shiftedNode) return undefined;
 
         if (this.#length === 1) {
-            this.head = this.tail = null;
+            this.tail = null;
+            this.head = this.tail;
         } else {
             this.head = this.head.next;
-            this.head.prev = null;
             shiftedNode.next = null;
         }
         this.#length--;
         return shiftedNode;
     }
+
+    /* Remove the first node with the given value: O(n) */
+    remove(value) {
+        let current = this.head;
+        if (!current) return false;
+
+        // If the value is at the head, remove it directly
+        if (current.value === value) {
+            this.shift();
+            return true;
+        }
+
+        // Traverse to find the node whose next has the target value
+        while (current.next && current.next.value !== value) {
+            current = current.next;
+        }
+
+        const nodeToRemove = current.next;
+
+        // If the value was not found, return false
+        if (!nodeToRemove) return false;
+
+        // Bypass the node to remove
+        current.next = nodeToRemove.next;
+
+        // If the removed node is not the tail, fix the backward link
+        if (nodeToRemove.next) {
+            nodeToRemove.next.prev = current;
+        } else {
+            // If it was the tail, update the tail reference
+            this.tail = current;
+        }
+
+        this.#length--;
+        return true;
+    }
+
+    /* O(values.length) */
+    build(values) {
+        for (const value of values) {
+            this.push(value);
+        }
+
+        return this;
+    }
 }
 
-module.exports = DoublyLinkedList;
+export { DoublyLinkedList };
